bc695aa681dc23844c36a9cc39343fb56be63a17
s472033 <usha.shrestha@stud-mail.uni-wuerzburg.de>
2026-03-14 15:13:39 +0100

    added transform for augment to resize

--- ab/nn/transform/base/norm_256.py
+++ ab/nn/transform/base/norm_256.py
@@ -4,5 +4,6 @@ import torchvision.transforms as transforms
 def transform(norm):
     return transforms.Compose([
         transforms.Resize(256),
+        transforms.CenterCrop(256),
         transforms.ToTensor(),
         transforms.Normalize(*norm)])

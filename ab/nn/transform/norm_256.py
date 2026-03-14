fec024b1a9cc36b5508aa276f49cf6aba6d83e42
Usha Shrestha <41979989+u-shrestha@users.noreply.github.com>
2026-03-14 16:28:25 +0100

    Update norm_256.py
    issue fix

--- ab/nn/transform/norm_256.py
+++ ab/nn/transform/norm_256.py
@@ -3,6 +3,6 @@ import torchvision.transforms as transforms
 
 def transform(norm):
     return transforms.Compose([
-        transforms.Resize(256),
+        transforms.Resize((256, 256)),
         transforms.ToTensor(),
         transforms.Normalize(*norm)])
